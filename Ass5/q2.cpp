6ec9c99f22fc1a8ab497ec3124c47752ea471b04
Gopal Kataria <gopal.kataria@research.iiit.ac.in>
2024-04-23 09:26:34 +0530

    Ass 5 q2 speed optimizations

--- Ass5/q2.cpp
+++ Ass5/q2.cpp
@@ -3,6 +3,45 @@ using namespace std;
 
 #define lint long long int
 
+void merge(vector<pair<int, int>> &arr, int l, int m, int r)
+{
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    vector<pair<int, int>> L(n1), R(n2);
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i].second >= R[j].second)
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+
+    while (i < n1)
+        arr[k++] = L[i++];
+
+    while (j < n2)
+        arr[k++] = R[j++];
+}
+
+void mergeSort(vector<pair<int, int>> &arr, int l, int r)
+{
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
 int main()
 {
 
@@ -25,9 +64,8 @@ int main()
         input.push_back({a, b});
     }
 
-    sort(input.begin(), input.end(), [](const pair<int, int> &a, const pair<int, int> &b)
-         { return a.second > b.second; });
     k = input.size() - 1;
+    mergeSort(input, 0, k);
 
     vector<lint> dist(n + 1);
     vector<bool> visited(n + 1);
@@ -35,11 +73,11 @@ int main()
     map<lint, lint> fibres;
     lint count = 0;
 
-djikstra:
     dist.assign(n + 1, LLONG_MAX);
-    visited.assign(n + 1, false);
     dist[1] = 0;
     pq.push({0, 1});
+djikstra:
+    visited.assign(n + 1, false);
     while (!pq.empty())
     {
         lint u = pq.top().second;
@@ -69,6 +107,7 @@ djikstra:
     
 
 justif:
+    pq.push({0, 1});
     if (k >= 0)
     {
         tie(a, b) = input[k--];
@@ -87,6 +126,7 @@ justif:
                 fibres[a] = b;
                 graph[a].push_back({1, b});
                 graph[1].push_back({a, b});
+                pq.push({b, a});
                 goto djikstra;
             }
         }
@@ -99,6 +139,7 @@ justif:
                 fibres[a] = b;
                 graph[a].push_back({1, b});
                 graph[1].push_back({a, b});
+                pq.push({b, a});
                 goto djikstra;
             }
             else
