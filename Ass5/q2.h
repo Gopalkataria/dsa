0c903c1717d1a7e3f89c3f37936848010cd4fc40
Gopal Kataria <gopal.kataria@research.iiit.ac.in>
2024-04-23 09:40:59 +0530

    Ass 5 : refactoring

--- Ass5/q2.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-void findping(std::vector<long long> &dist, std::priority_queue<std::pair<long long, long long>, std::vector<std::pair<long long, long long>>, std::greater<std::pair<long long, long long>>> &pq, std::vector<bool> &visited, std::vector<std::vector<std::pair<long long, long long>>> &graph);
