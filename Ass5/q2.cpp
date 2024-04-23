91fbf1062a86e4ac207d47aea21d8c549f716797
Gopal Kataria <gopal.kataria@research.iiit.ac.in>
2024-04-23 09:01:40 +0530

    Ass 5 q2 should work now ig ?

--- Ass5/q2.cpp
+++ Ass5/q2.cpp
@@ -18,11 +18,26 @@ int main()
         graph[b].push_back({a, l});
     }
 
+    vector<pair<int, int>> input;
+    while (k--)
+    {
+        cin >> a >> b;
+        input.push_back({a, b});
+    }
 
-    vector<lint> dist(n + 1, INT64_MAX);
-    vector<bool> visited(n + 1, false);
+    sort(input.begin(), input.end(), [](const pair<int, int> &a, const pair<int, int> &b)
+         { return a.second > b.second; });
+    k = input.size() - 1;
+
+    vector<lint> dist(n + 1);
+    vector<bool> visited(n + 1);
     priority_queue<pair<lint, lint>, vector<pair<lint, lint>>, greater<pair<lint, lint>>> pq;
+    map<lint, lint> fibres;
+    lint count = 0;
 
+djikstra:
+    dist.assign(n + 1, LLONG_MAX);
+    visited.assign(n + 1, false);
     dist[1] = 0;
     pq.push({0, 1});
     while (!pq.empty())
@@ -47,27 +62,49 @@ int main()
         }
     }
 
-
-    lint count = 0;
-    map<lint, lint> fibres;
-    while (k--)
+    for (lint i = 1; i < n+1; i++)
     {
-        cin >> a >> b;
+        cout << i << "-" << dist[i] << "\n";
+    }
+    
+
+justif:
+    if (k >= 0)
+    {
+        tie(a, b) = input[k--];
+        cout << a << " " << b << endl;
         if (fibres.find(a) == fibres.end())
         {
             l = dist[a];
             if (l <= b)
             {
                 count++;
-                fibres[a] = l;
+                goto justif;
+                // cout << a << " " << b << " " << l << endl;
             }
             else
+            {
                 fibres[a] = b;
+                graph[a].push_back({1, b});
+                graph[1].push_back({a, b});
+                goto djikstra;
+            }
         }
         else
         {
-            fibres[a] = min(fibres[a], b);
+            // cout << a << " " << b << " " << l << endl;
             count++;
+            if( fibres[a] > b)
+            {
+                fibres[a] = b;
+                graph[a].push_back({1, b});
+                graph[1].push_back({a, b});
+                goto djikstra;
+            }
+            else
+            {
+                goto justif;
+            }
         }
     }
 
