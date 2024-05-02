826e5086c310e6d63bbe6c00fd92a91c55ef9b5e
Gopal Kataria <gopal.kataria@research.iiit.ac.in>
2024-05-02 22:23:45 +0530

    Ass 5 : Too much optimization

--- Ass5/mw.c
+++ Ass5/mw.c
@@ -2,75 +2,44 @@
 #include <stdlib.h>
 #include <stdbool.h>
 typedef long long int ll;
-ll *Uconn;
-ll *Usize;
 
-bool issame(ll p, ll q)
+ll find(ll p, ll *Uconn)
 {
     while (p != Uconn[p])
-    {
         p = Uconn[p];
-    }
-    while (q != Uconn[q])
-    {
-        q = Uconn[q];
-    }
-    return (p == q) ? true : false;
+    return p;
 }
-ll conn(ll p, ll q)
+bool issame(ll p, ll q, ll *Uconn, ll *Usize)
 {
-    while (p != Uconn[p])
-    {
-        p = Uconn[p];
-    }
-    while (q != Uconn[q])
-    {
-        q = Uconn[q];
-    }
-    ll c = Usize[p] > Usize[q] ? p : q;
-    ll d = Usize[p] > Usize[q] ? q : p;
+    return (find(p, Uconn) == find(q, Uconn)) ? true : false;
+}
+ll conn(ll a, ll b, ll *Uconn, ll *Usize)
+{
+    ll p = find(a, Uconn), q = find(b, Uconn);
+    ll c = (Usize[p] > Usize[q] ? p : q), d = (Usize[p] > Usize[q] ? q : p);
     Usize[c] += Usize[d];
     Uconn[d] = c;
     return d;
 }
 int main()
 {
-    ll n, m;
+    ll n, m, p, q;
     scanf("%lld %lld", &n, &m);
-    Uconn = (ll *)calloc((n + 1), sizeof(ll));
-    Usize = (ll *)calloc((n + 1), sizeof(ll));
+    ll count = n - 1, i = 0, printarr[n + 1];
+    ll Uconn[n + 1], heads[n + 1], Usize[n + 1];
     for (ll i = 0; i <= n; i++)
-    {
-        Uconn[i] = i;
-        Usize[i] = 1;
-    }
-    ll *heads = (ll *)calloc((n + 1), sizeof(ll));
-    ll count = n - 1;
-    ll p, q;
+        Uconn[i] = i * (Usize[i] = 1 + (heads[i] = 0));
     for (ll x = 0; x < m; x++)
     {
         scanf("%lld %lld", &p, &q);
-        if (!(issame(p, q)))
-        {
-            ll k = conn(p, q);
-            heads[k] = 1;
-            count--;
-        }
+        if (!(issame(p, q, Uconn, Usize)))
+            count -= (heads[conn(p, q, Uconn, Usize)] = 1);
     }
     printf("%lld\n", count);
-    ll i = 0;
-    ll printarr[n + 1];
-
     for (ll x = 1; x < n + 1; x++)
-    {
         if (heads[x] == 0)
-        {
             printarr[i++] = x;
-        }
-    }
     for (ll x = 1; x < i; x++)
-    {
         printf("%lld %lld\n", printarr[x - 1], printarr[x]);
-    }
     return 0;
 }
