5032435d258e32111f69d0029f64f2bcc8eb5af1
Gopal Kataria <gopal.kataria@research.iiit.ac.in>
2024-05-02 23:00:39 +0530

    Ass 5 : lines minimized to the level of code obfustication

--- Ass5/mw.c
+++ Ass5/mw.c
@@ -1,45 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-typedef long long int ll;
-
-ll find(ll p, ll *Uconn)
+//  I really wish I could submit this code for assignments without being flagged for code obfustication
+long long int find(long long int p, long long int *Uconn)
 {
-    while (p != Uconn[p])
-        p = Uconn[p];
+    while (p != Uconn[p])  p = Uconn[p];
     return p;
 }
-bool issame(ll p, ll q, ll *Uconn, ll *Usize)
+long long int conn(long long int a, long long int b, long long int *Uconn, long long int *Usize)
 {
-    return (find(p, Uconn) == find(q, Uconn)) ? true : false;
-}
-ll conn(ll a, ll b, ll *Uconn, ll *Usize)
-{
-    ll p = find(a, Uconn), q = find(b, Uconn);
-    ll c = (Usize[p] > Usize[q] ? p : q), d = (Usize[p] > Usize[q] ? q : p);
-    Usize[c] += Usize[d];
-    Uconn[d] = c;
+    long long int p = find(a, Uconn), q = find(b, Uconn);
+    long long int c = (Usize[p] > Usize[q] ? p : q), d = (Usize[p] > Usize[q] ? q : p);
+    Usize[c] += Usize[d] * (Uconn[d] = c) / c ;
     return d;
 }
 int main()
 {
-    ll n, m, p, q;
+    long long int n, m, i = 0, p, q;
     scanf("%lld %lld", &n, &m);
-    ll count = n - 1, i = 0, printarr[n + 1];
-    ll Uconn[n + 1], heads[n + 1], Usize[n + 1];
-    for (ll i = 0; i <= n; i++)
-        Uconn[i] = i * (Usize[i] = 1 + (heads[i] = 0));
-    for (ll x = 0; x < m; x++)
+    long long int count = n - 1, printarr[n + 1], Uconn[n + 1], heads[n + 1], Usize[n + 1];
+    for (long long int i = 0; i <= n; i++) Uconn[i] = i * (Usize[i] = 1 + (heads[i] = 0));
+    for (long long int x = 0; x < m; x++)
     {
         scanf("%lld %lld", &p, &q);
-        if (!(issame(p, q, Uconn, Usize)))
-            count -= (heads[conn(p, q, Uconn, Usize)] = 1);
+        if (!(find(p, Uconn) == find(q, Uconn))) count -= (heads[conn(p, q, Uconn, Usize)] = 1);
     }
     printf("%lld\n", count);
-    for (ll x = 1; x < n + 1; x++)
-        if (heads[x] == 0)
-            printarr[i++] = x;
-    for (ll x = 1; x < i; x++)
-        printf("%lld %lld\n", printarr[x - 1], printarr[x]);
+    for (long long int x = 1; x < n + 1; x++)  if ((heads[x] == 0) )  printarr[i++] = x;
+    for (long long int x = 1; x < i; x++)  printf("%lld %lld\n", printarr[x - 1], printarr[x]);
     return 0;
 }
