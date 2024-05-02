dbaf10e6b2467f28508918c934b776c543ea24f0
Gopal Kataria <gopal.kataria@research.iiit.ac.in>
2024-05-02 23:12:34 +0530

    Ass 5 : Brain Pain Code

--- Ass5/mw.c
+++ Ass5/mw.c
@@ -2,29 +2,29 @@
 //  I really wish I could submit this code for assignments without being flagged for code obfustication
 long long int find(long long int p, long long int *Uconn)
 {
-    while (p != Uconn[p])  p = Uconn[p];
+    for (; p != Uconn[p]; p = Uconn[p]) ;
     return p;
 }
 long long int conn(long long int a, long long int b, long long int *Uconn, long long int *Usize)
 {
-    long long int p = find(a, Uconn), q = find(b, Uconn);
+    long long int p = find(a, Uconn),                q = find(b, Uconn);
     long long int c = (Usize[p] > Usize[q] ? p : q), d = (Usize[p] > Usize[q] ? q : p);
     Usize[c] += Usize[d] * (Uconn[d] = c) / c ;
     return d;
 }
 int main()
 {
-    long long int n, m, i = 0, p, q;
+    long long int n, m, i = 0 , j = 0 , p, q , x ;
     scanf("%lld %lld", &n, &m);
-    long long int count = n - 1, printarr[n + 1], Uconn[n + 1], heads[n + 1], Usize[n + 1];
-    for (long long int i = 0; i <= n; i++) Uconn[i] = i * (Usize[i] = 1 + (heads[i] = 0));
-    for (long long int x = 0; x < m; x++)
+    long long int count = n - 1, Uconn[n + 1],     Usize[n + 1],   heads[n + 1], printarr[n + 1];
+    for ( i = 0; i <= n; i++)    Uconn[i] = i *   (Usize[i] = 1 + (heads[i] = 0));
+    for ( x = 0; x < m; x++)
     {
         scanf("%lld %lld", &p, &q);
-        if (!(find(p, Uconn) == find(q, Uconn))) count -= (heads[conn(p, q, Uconn, Usize)] = 1);
+        if (find(p, Uconn) != find(q, Uconn)) count -= (heads[conn(p, q, Uconn, Usize)] = 1);
     }
     printf("%lld\n", count);
-    for (long long int x = 1; x < n + 1; x++)  if ((heads[x] == 0) )  printarr[i++] = x;
-    for (long long int x = 1; x < i; x++)  printf("%lld %lld\n", printarr[x - 1], printarr[x]);
+    for ( x = 1; x < n + 1; x++)  if(heads[x] == 0)     printarr[j++] = x;
+    for ( x = 1; x < j; x++)      printf("%lld %lld\n", printarr[x - 1],  printarr[x]);
     return 0;
 }
